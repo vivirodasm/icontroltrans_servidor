acbe233a6179252f26b71b8235710a2370c8061a
Oscar-DLopez <36933192+Oscar-DLopez@users.noreply.github.com>
2019-12-24 10:27:25 -0500

    Update extractos.js

--- web/js/extractos.js
+++ web/js/extractos.js
@@ -3,7 +3,7 @@ $( document ).ready(function()
 	
 	$("[data-select2-id='tbextractos-idtercero']").prop( "disabled", false );
 	// $("[data-select2-id='tbextractos-idtercero']").prop( "disabled", false );
-	if( '/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i'.test(navigator.userAgent) ) 
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) 
 	{
 		console.log('Esto es un dispositivo móvil');
 		$("[data-select2-id='tbextractos-idtercero']").prop( "disabled", true );
